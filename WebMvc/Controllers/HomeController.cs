8804688cb5909b3c3531848387fadf444d71f2f5
Bruno Marques <b_marques85@live.com>
2021-06-08 12:38:52 -0300

    Departmets criado

--- WebMvc/Controllers/HomeController.cs
+++ WebMvc/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WebMvc.Models;
+using WebMvc.Models.ViewModels;
 
 namespace WebMvc.Controllers
 {
