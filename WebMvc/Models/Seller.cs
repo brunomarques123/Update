d28bc09ea29ef8e8a47a3b3c0c76be238b5a95e3
Bruno Marques <b_marques85@live.com>
2021-06-14 19:48:52 -0300

    formatacao numero, local,data

--- WebMvc/Models/Seller.cs
+++ WebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace WebMvc.Models
@@ -8,8 +9,18 @@ namespace WebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Birthdate { get; set; }
+
+        [Display(Name="Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:f2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
