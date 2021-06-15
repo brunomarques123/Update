6d5c2e476570861c00e9217c74c255a7668ae20f
Bruno Marques <b_marques85@live.com>
2021-06-15 16:03:20 -0300

    Arrumando delete com Exception

--- WebMvc/Services/SellerService.cs
+++ WebMvc/Services/SellerService.cs
@@ -35,10 +35,18 @@ namespace WebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-           await _context.SaveChangesAsync();
-        }
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Nao posso deletar o vendedor");
+            }
+
+            }
 
         public async Task UpdateAsync(Seller obj)
         {
@@ -53,11 +61,11 @@ namespace WebMvc.Services
                 await _context.SaveChangesAsync();
 
             }
-            catch(DbUpdateConcurrencyException e)
+            catch (DbUpdateConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
             }
 
-            }
         }
     }
+}
